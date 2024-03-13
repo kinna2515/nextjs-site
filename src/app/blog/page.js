2d4ec6aca4a42c7ed06f07e63cb311f843882660
Inna Barysenka <inna.barysenka@gmail.com>
2024-03-13 17:01:40 +0300

    feat: add links for pages (go tothe blog page and back

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,14 +1,18 @@
+import Link from "next/link";
 export default function Blog() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-700 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-600 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <main className="flex min-h-screen flex-col items-start justify-start pl-20 pr-20 pt-10 pb-10">
+      <div className="flex w-full justify-start border-b border-gray-600 pb-6">
+        <Link href="/">Menu</Link>
+      </div>
+      <div className="flex flex-col w-full items-center justify-center place-items-center gap-10 mt-20  font-mono text-sm lg:flex">
+        <div className=" flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-700 pb-6 pt-8 mt-15 backdrop-blur-2xl dark:border-neutral-600 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <h2>Women in tech Blog</h2>
         </div>
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-700 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-600 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <div className=" flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-700 pb-6 pt-8 mt-15 backdrop-blur-2xl dark:border-neutral-600 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <h4>Next.js course in Women tech project is started</h4>
         </div>
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-700 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-600 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <div className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-700 pb-6 pt-8 m-15 backdrop-blur-2xl dark:border-neutral-600 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <h3>Feedbacks</h3>
         </div>
       </div>
